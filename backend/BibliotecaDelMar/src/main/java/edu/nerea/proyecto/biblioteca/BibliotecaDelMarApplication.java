713e91b24b3a8d099c7d1ff1089b774f2c02d1c1
nekoo <nereapena42gmail.com>
2022-05-17 15:33:11 +0200

    Creacion proyecto con Spring + añadiendo vistas y ajustando rutas

---
+++ backend/BibliotecaDelMar/src/main/java/edu/nerea/proyecto/biblioteca/BibliotecaDelMarApplication.java
@@ -0,0 +1,13 @@
+package edu.nerea.proyecto.biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaDelMarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaDelMarApplication.class, args);
+	}
+
+}
---
+++ backend/BibliotecaDelMar/src/test/java/edu/nerea/proyecto/biblioteca/BibliotecaDelMarApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nerea.proyecto.biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaDelMarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

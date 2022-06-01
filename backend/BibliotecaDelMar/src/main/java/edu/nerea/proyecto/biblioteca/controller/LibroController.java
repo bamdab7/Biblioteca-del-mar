f91fe7b7d5d48ef9ff77e5f37f17caf63a0aeb2d
ECOMT\npena <npena@ecomt.net>
2022-06-01 17:13:26 +0200

    Creacion de metodos guardar y crear, junto con manejo de errores del formulario y mensajes informativos

--- backend/BibliotecaDelMar/src/main/java/edu/nerea/proyecto/biblioteca/controller/LibroController.java
+++ backend/BibliotecaDelMar/src/main/java/edu/nerea/proyecto/biblioteca/controller/LibroController.java
@@ -5,8 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import edu.nerea.proyecto.biblioteca.entity.Libro;
 import edu.nerea.proyecto.biblioteca.service.ILibroService;
 
@@ -31,7 +37,41 @@ public class LibroController {
 		model.addAttribute("libro", libro);
 		return "catalogo/detalle.html";
 	}
-	
+
+		//esta ruta nos permitira crear productos FALTA IMAGEN Y AUTOR Y EDITORIAL
+	@GetMapping("/createLibro")
+	public String crearLibro(){
+		return "forms/addLibro.html";
+	}
+
+	@PostMapping("/saveLibro") //BindingResult para capturar los errores de conversion del formulario
+	public String crearLibro(Libro libro,BindingResult result, RedirectAttributes attributes){
+		if(result.hasErrors()){
+			for(ObjectError error : result.getAllErrors()){
+				System.out.println("Error: " + error.getDefaultMessage());
+			} //nos informa que error se esta cometiendo y nos devuelve al formulario
+			return "form/addLibro.html";
+		}
+		serviceLibros.guardarLibro(libro);
+	//	List<Libro> lista = serviceLibros.buscarTodosLibros();
+	//	model.addAttribute("libros", lista); //asi una vez guardado pille los libros y los liste
+		attributes.addFlashAttribute ("msg", "Se ha guardado el producto correctamente");
+		return "catalogo/productos.html";
+	}
+
+		//a revisar
+	@GetMapping("/editLibro/{id}")
+	public String editLibro(@PathVariable("id") int idLibro,Model model){
+		Libro libro=serviceLibros.buscarPorId(idLibro);
+		model.addAttribute("libro",libro);
+		return "forms/addLibro.html";
+	}
+
+	@GetMapping("/eliminarLibro/{id}")
+	public String eliminarLibro(@RequestParam("id") int idLibro, Model model){
+		model.addAttribute("libro", idLibro);
+		return null;
+	}
 
 	//pageable
 }

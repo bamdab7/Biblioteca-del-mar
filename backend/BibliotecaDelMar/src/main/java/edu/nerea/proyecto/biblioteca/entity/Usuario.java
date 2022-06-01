0d519741b7c8cc9a182bd3109ee569eb649317e3
ECOMT\npena <npena@ecomt.net>
2022-06-01 09:33:09 +0200

    Añadiendo getters and setters usuario

--- backend/BibliotecaDelMar/src/main/java/edu/nerea/proyecto/biblioteca/entity/Usuario.java
+++ backend/BibliotecaDelMar/src/main/java/edu/nerea/proyecto/biblioteca/entity/Usuario.java
@@ -19,7 +19,54 @@ public class Usuario {
 	private String password;
 	private Integer status;
 	
-	
+	public Usuario(){
+		super();
+	}
+
+	public Integer getId(){
+		return id;
+	}
+	public void setId(Integer id){
+		this.id= id;
+	}
+	public String getNombre(){
+		return nombre;
+	}
+	public void setNombre(String nombre){
+		this.nombre=nombre;
+	}
+	public String setEmail(){
+		return email;
+	}
+	public void getEmail(String email){
+		this.email=email;
+	}
+	public String setUsername(){
+		return username;
+	}
+	public void getUsername(String username){
+		this.username=username;
+	}
+	public String setPassword(){
+		return password;
+	}
+	public void getPassword(String password){
+		this.password=password;
+	}
+	public Integer setStatus(){
+		return status;
+	}
+	public void getStatus(Integer status){
+		this.status=status;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", nombre=" + nombre + ", email=" + email + ", username=" + username + ", password="
+		+ password + ", status=" + status +  "]";
+	}
+
+
 	
 	
 }

bb97e249b3370e60d48f77077512c48fa63f49c6
nekoo <nereapena42gmail.com>
2022-06-04 23:19:04 +0200

    Añadiendo seguridad, ajustando para que se vea siempre el css + img

--- backend/BibliotecaDelMar/src/main/java/edu/nerea/proyecto/biblioteca/security/DatabaseWebSecurity.java
+++ backend/BibliotecaDelMar/src/main/java/edu/nerea/proyecto/biblioteca/security/DatabaseWebSecurity.java
@@ -5,6 +5,7 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -22,7 +23,26 @@ public class DatabaseWebSecurity extends WebSecurityConfigurerAdapter {
 		.authoritiesByUsernameQuery("select u.username, p.perfil from usuarioperfil up " + 
 				"inner join usuarios u on u.id = up.idUsuario "+ 
 				"inner join perfiles p on p.id = up.idPerfil "+
-				"where u.username = ?");
+				"where u.username = ?");	
+	}
+	
+	//personalizacion de acceso a las url
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/resources/**",
+											"/static/**",
+											"/img/**",
+											"/css/**").permitAll().antMatchers("/",
+																				"/signup",
+																				"/actividad1",
+																				"/actividad2",
+																				"/actividad3",
+																				"/noticias",
+																				"/FA",
+																				"/aboutus",
+																				"/resources/**",
+																				"/static/**"
+																				).permitAll().anyRequest().authenticated().and().formLogin().permitAll();	
 		
 	}
 }
